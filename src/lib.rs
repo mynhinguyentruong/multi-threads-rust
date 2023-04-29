5e3548817c95d35a0f1df73fde60f07081e540f8
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-04-29 13:55:14 -0400

    add execute

--- src/lib.rs
+++ src/lib.rs
@@ -4,44 +4,68 @@ use std::thread::{Builder,JoinHandle, Thread};
 use std::time::Duration;
 
 pub struct ThreadPool {
-    pub(crate) workers: Vec<Worker>,
+    workers: Vec<Worker>,
     sender: mpsc::Sender<Job>
 }
 
 struct Worker {
     id: usize,
-    thread : JoinHandle<Arc<Mutex<Receiver<Job>>>>,
+    thread : JoinHandle<()>,
 }
 
-struct Job {
-    // hold the closure we want to send down channel
-}
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
 
 impl Worker {
 
     pub fn new(id: usize, receiver: Arc<Mutex<Receiver<Job>>>) -> Worker {
-        let builder: Builder = Builder::new();
-        let thread = builder.spawn(|| {
-            receiver
+        let builder: Builder = Builder::new().name("my-thread".into()).stack_size(1024 * 1024);
+        let thread = builder.spawn(move || loop {
+            let result = receiver.lock().unwrap().recv();
+            match result {
+                Ok(job) => {
+                    println!("Worker {id} Successfully handle job");
+                    job();
+                },
+                Err(_) => {
+                    println!("Worker {id} disconnected, shutting down");
+                    break;
+                }
+            }
+
         }).unwrap();
         return Worker { id, thread }
     }
+
 }
 
 impl ThreadPool {
 
+// pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {}
+
     pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
         let mut workers = Vec::with_capacity(size);
 
         let (sender, receiver) = mpsc::channel();
 
         let receiver = Arc::new(Mutex::new(receiver));
 
-        for id in 0..size {
+    for id in 0..size {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
         return ThreadPool { workers, sender }
 
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job: Job = Box::new(f);
+
+        self.sender.send(job).unwrap();
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,66 @@
 
-mod canada;
+// mod canada;
 mod lib;
 
 use::std::thread;
-use::std::time::Duration;
+// use::std::time::Duration;
 
-use std::alloc::System;
-use std::time::SystemTime;
+// use std::alloc::System;
+// use std::time::SystemTime;
 
-use std::sync::{Arc, mpsc, Mutex};
-use std::rc::Rc;
+// use std::sync::{Arc, mpsc, Mutex};
+// use std::rc::Rc;
 
 use std::{fs, io::{prelude::*, BufReader}, net::{TcpStream, TcpListener}};
 
-use chrono::Utc;
-use chrono::TimeZone;
-use chrono::Date;
-use chrono::{Duration as ChronoDuration, NaiveDate};
-use crate::lib::{ThreadPool, Worker};
+// use chrono::Utc;
+// use chrono::TimeZone;
+// use chrono::Date;
+// use chrono::{Duration as ChronoDuration, NaiveDate};
+use lib::{ThreadPool};
 
 fn main() {
 
-    let pool = ThreadPool::new(3);
 
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    let pool = ThreadPool::new(5);
 
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        })
+    }
+
+}
+
+fn handle_connection(mut stream: TcpStream) {
+
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, file_name) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let content = fs::read_to_string(file_name).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line, content.len(), content
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap()
 }
 
 
